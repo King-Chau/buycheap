472d3fba3615adb0d4340f435a2cc11581d09c88
oujingzhou <oujingzhou@hotmail.com>
2024-03-30 18:09:00 +0800

    pre-line

--- pages/index.js
+++ pages/index.js
@@ -126,7 +126,7 @@ export default function components() {
                 </div>
                     <CardContent>
                         <div className="mt-4 rounded-lg p-4">
-                            <p className="ml-2 text-lg" style={{ textAlign: 'center', whiteSpace: 'pre-line' }}>
+                            <p className="ml-2 text-lg" style={{ textAlign: 'center' }}>
                                 {compressing
                                     ? '👩🏻'
                                     : loading
