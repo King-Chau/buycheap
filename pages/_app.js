1a599b2857512ba9595b85ad49c2cd16e97ea851
oujingzhou <oujingzhou@stepfun.com>
2025-06-11 17:39:07 +0800

    add vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css'Add commentMore actions
 import { Fragment } from 'react'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
     return (
         <Fragment>
             <Component {...pageProps} />
+            <Analytics />
         </Fragment>
-    )
+    )Add commentMore actions
 }
 
 export default MyApp
